ee84bd2de87e785834b597e0f3ec149ea3473f3d
Hebaabbas <heba1996abbas@gmail.com>
2023-07-15 22:47:43 +0000

    add the get_sold_data function that lets the user add the value sold for the day

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from datetime import datetime
+
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -15,3 +17,21 @@ market_name = input("Hello! This is a Fruit market store data collector. Please 
 user_age= int(input("And now please input your age: "))
 if user_age < 18:
     raise SystemExit('The user must be at least 18')
+
+
+def get_sold_data():
+
+        today = datetime.now().date()
+
+        print("Welcome to " + market_name.capitalize() + "'s sales data collector.")
+        print("Please enter how many products were sold on the date of " + str(today))
+        print("Data provided are to be 8 values for different fruits kind, separated by commas.")
+        print("Data provided represents these products in this order: [Strawberry ,Apple ,Banana ,Mango ,Avocado ,Orange ,Kiwi ,Lemon] and the maximum amount of products per item we can sell each day is 30. ")
+        print("Data shold be as this Example: 11,11,11,11,11,11,11,11\n")
+
+        data_str = input("Enter your data values here: \n")
+        print(f"The data provided is {data_str}")
+
+get_sold_data()
+
+
